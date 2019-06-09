14d9e62242df4829e16231760bb8e22fdfb54bd8
Callens Ben <callens.ben@github.com>
2019-06-09 17:17:22 +0200

    Initial commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        $title = 'Wecome To Laravel!';
+        //return view('pages.index', compact('title'));
+        return view('pages.index')->with('title', $title);
+    }
+
+    public function about(){
+        $title = 'About Us!';
+        return view('pages.about')->with('title', $title);
+    }
+    public function services(){
+        $data = array (
+            'title' => 'Services!',
+            'services' => ['Web Design', 'Programming', 'SEO']
+        );
+        return view('pages.services')->with($data);
+    }
+}
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+        <h1>{{$title}}</h1>
+        <p>This is the about page</p>
+    </body>
+@endsection
